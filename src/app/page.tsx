9b3ade661563b810153564711ce95c70f0713a1c
apo1798 <nploxz@gmail.com>
2023-06-07 14:51:53 +0800

    add style not following prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const newContent = '^_^!';
+  const newContent                             = '^_^!'
+
+
+  ;
   console.log(newContent);
 
   return (
